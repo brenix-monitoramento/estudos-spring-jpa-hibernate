65b6dd637c0c833249282ee43d02b4dac3118d41
Breno Gonçalves <breno.goncalves@datatraffic.com.br>
2026-07-07 10:22:43 -0300

    adiciona estrutura de projeto com Maven

---
+++ aprendizado-1/src/models/Pessoa.java
@@ -0,0 +1,37 @@
+package models;
+
+public class Pessoa {
+    private Integer id;
+    private String nome;
+    private String email;
+
+    Pessoa(Integer id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String id) {
+        this.email = email;
+    }
+}
