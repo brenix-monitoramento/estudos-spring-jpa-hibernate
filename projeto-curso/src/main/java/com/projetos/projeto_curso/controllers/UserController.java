5cfc4aa784255138c0511dfc238820c7570b5254
Breno Gonçalves <breno.goncalves@datatraffic.com.br>
2026-07-17 16:16:06 -0300

    modifica UserController

--- projeto-curso/src/main/java/com/projetos/projeto_curso/controllers/UserController.java
+++ projeto-curso/src/main/java/com/projetos/projeto_curso/controllers/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -18,9 +18,7 @@ public class UserController {
         var user1 = new User(1L, "Breno", "breno@mail.com", "999999", "12345.");
         var user2 = new User(2L, "Luana", "luana@mail.com", "454545", "65765867.");
 
-        List<User> users = new ArrayList<>();
-        users.add(user1);
-        users.add(user2);
+        var users = Arrays.asList(user1, user2);
 
         return ResponseEntity.ok().body(users);
     }
